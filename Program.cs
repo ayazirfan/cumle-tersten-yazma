bd849bc3329e90d601bff2dca433187118e93ff6
irfan ayaz <byirfanayaz@gmail.com>
2024-05-30 13:28:58 +0300

    string cümleleri ters çevirme

--- Program.cs
+++ Program.cs
@@ -9,4 +9,16 @@ for (int i = 0; i < cumle.Length; i++)
 }
 Console.WriteLine("{0}", ters);
 
-System.Console.WriteLine("gitdiğiniz cümlenin tersi:" + new string (cumle.Reverse().ToArray()));
+System.Console.WriteLine("gitdiğiniz cümlenin tersi:" + new string (cumle.Reverse().ToArray()));Console.Write("Bir sayi giriniz: ");
+            
+
+            // int sayi = Convert.ToInt32(Console.ReadLine()); * girilen sayıyı ter çevirme
+            // int rakam;
+            // Console.Write("Sayinin tersten yazılısı = ");
+            // while (sayi > 0)
+            // {
+            //     rakam = sayi % 10;
+            //     Console.Write(rakam);
+            //     sayi /= 10;
+            // }
+            
